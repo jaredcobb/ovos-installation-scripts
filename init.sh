6824c37b804b88e70f4eff468364d8125a9636e3
Jared Cobb <github@jaredcobb.com>
2024-06-01 18:29:15 -0400

    remove packages

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 sudo apt -y update && sudo apt -y upgrade
 
-sudo apt install -y build-essential python3-dev python3-pip python3-venv \
-    swig libssl-dev libfann-dev portaudio19-dev \
-    libpulse-dev cmake libncurses-dev pulseaudio-utils pulseaudio \
-    espeak-ng mpg123 speedtest-cli vim git
+sudo apt install -y speedtest-cli vim git
 
 sudo wget -O /etc/motd https://gist.githubusercontent.com/jaredcobb/304281217b15da56c7a4b1c2e3c8e68f/raw/f10ec781d5d57012a1c029e02ddfb5f45ee493c4/motd
 
