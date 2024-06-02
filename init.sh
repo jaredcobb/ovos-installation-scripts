391da07ebce08feaf5dac620cd09982e8703b1a8
Jared Cobb <github@jaredcobb.com>
2024-06-02 17:27:39 -0400

    Add additional init changes

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+sudo raspi-config nonint do_change_locale en_US.UTF-8
+sudo raspi-config nonint do_spi 0
+sudo raspi-config nonint do_i2c 0
+
 sudo apt -y update && sudo apt -y upgrade
 
 sudo apt install -y speedtest-cli vim git
